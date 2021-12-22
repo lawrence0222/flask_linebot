211bb92a164c503d6ca97f7d38180e4a51237e94
lawrence0222 <lawrence02222@gmail.com>
2021-12-23 03:55:22 +0800

    make it better

--- manager/routes.py
+++ manager/routes.py
@@ -1,9 +1,19 @@
+from re import S
 from flask import render_template, request, redirect, url_for
 import requests
 import json
 from flask import Blueprint
 from models import *
+from linebot.models import TextSendMessage
+from linebot.exceptions import LineBotApiError
+from linebot import LineBotApi
+
+
+
+
 manager = Blueprint("manager", __name__,template_folder="templates", url_prefix='/manager')
+
+line_bot_api = LineBotApi('lZjUhkDwED79lPFD/0k68acKfkoxtxkS4rdsmK3epf4XuiYaqojTL72gqFxACeO4QtettJ0X+/GuTMJMxm/q7B/Cq1+qbZzp3H7oXQTCyfCmFvpACekbwJ8VeaTQE5kihkBDRLivsM0hSEcWcGGFDwdB04t89/1O/w1cDnyilFU=')
 
 
 @manager.route("/", methods=["POST", "GET"])
@@ -42,4 +52,5 @@ def submit3():
     user_id = request.form['nm']
     text = request.form['text']
     update_msg(user_id,text)
+    line_bot_api.push_message('%s', TextSendMessage(text='Hello World!') % (user_id))
     return render_template("manager.html",user_info= user_info())
